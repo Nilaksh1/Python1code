97d20f6c29d963a8b37e5b733f61f1d3df21cc93
Nilaksh1 <40692589+Nilaksh1@users.noreply.github.com>
2020-04-23 21:36:33 +0530

    Update BadFortune.py

--- BadFortune.py
+++ BadFortune.py
@@ -1,4 +1,4 @@
-mport struct, random, string
+import struct, random, string
 
 # C long variables are different sizes on 32-bit and 64-bit machines,
 # so we have to measure how big they are on the machine where this is running.
